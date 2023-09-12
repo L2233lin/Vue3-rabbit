a053258734956e59a95a01fc2293079806a3557b
L2233 <760142315@qq.com>
2023-09-12 10:47:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/stores/index.js
@@ -0,0 +1,15 @@
+import { createPinia } from 'pinia'
+import persist from 'pinia-plugin-persistedstate'
+
+//pinia 独立维护
+const pinia = createPinia()
+pinia.use(persist)
+
+export default pinia
+
+//仓库 统一导出
+export * from './modules/user'
+
+//export * from './modules/user' 等价于==>
+// 1. import { useUserStore } from './modules/user'
+// 2. export { useUserStore }
