f0962bf7e6301d8654188e299a492c9f239fc7d7
L2233 <760142315@qq.com>
2023-09-14 23:25:16 +0800

    home-新鲜好物与人气推荐实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,3 +1,7 @@
 import request from '@/utils/request'
 
 export const getBannerAPI = () => request.get('home/banner')
+
+export const getNewAPI = () => request.get('/home/new')
+
+export const getHotAPI = () => request.get('/home/hot')
