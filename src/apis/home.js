511a360b6e3769573d55b5afbec15c401aedfb50
L2233 <760142315@qq.com>
2023-09-15 16:52:59 +0800

    home-GoodsItem组件封装

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,5 @@ export const getBannerAPI = () => request.get('home/banner')
 export const getNewAPI = () => request.get('/home/new')
 
 export const getHotAPI = () => request.get('/home/hot')
+
+export const getGoodsAPI = () => request.get('/home/goods')
